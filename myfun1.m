231be00d0b62ed191a9dfb7551c3d6d96f79f0af
laogut <laogut@poczta.fm>
2015-12-28 15:52:53 +0100

    Gotowe aproksymowanie obiektem I rzędu

--- myfun1.m
+++ myfun1.m
@@ -1,15 +1,17 @@
 function err = myfun1(x)
-    global Y;
-    global t;
-    global stepTime
-
-    Kp = x(1);
-    T = x(2);
-    L = x(3)
+    global res;
+    global num;
+    global den;
+    global delay;
     
-    G = tf(Kp,[T 1],'InputDelay',L);
-    [YG, TG] = step(G,stepTime);
-    Yg = interp1(t, Y, TG,'pchip');
-    err = (YG - Yg);
-    err = sum(err.^2)
+    num = x(1);
+    den = [x(2) 1];
+    delay = x(3);
+    if delay < 0 
+        delay = 0;
+    end
+    sim('single_object'); 
+    res2 = Y.signals.values;
+    err = res - res2;
+    err = sum(err.^2);
 end
--- skrypt.m
+++ skrypt.m
@@ -2,180 +2,333 @@
 clear all;
 clc;
 close all;
+global res;
+global num;
+global den;
+global delay;
 
-global Y;
-global t;
-global stepTime;
-
-d = exp(-tf('s'));
 %% P1
 T = [0.02 0.05 0.1 0.2 0.3 0.5 0.7 1 1.3 1.5 2 4 6 8 10 20 50 100 200 500 1000];
-for i = 1:length(T)
-    [P1(i)] = d * tf(1,[T(i) 1]);
-end
-stepTime = 6000;
 
-hold on;
 for i = 1:length(T)
-    figure()
-    [Y,t] = step(P1(i),stepTime);
-    [X,FVAL] = fmincon(@myfun1,[0 0 0]',diag([0 0 -1]),[0 0 0]',[],[],[0 0 0]',[]);
-    G = tf(X(1),[X(2) 1],'InputDelay',X(3));
-    [Yg, Tg] = step(G,stepTime);
-    subplot(2,1,1)
-    plot (t,Y);
+    num = 1;
+    den = [T(i) 1 ];
+    delay = 1;
+    simTime = delay + 6*T(i);
+    if (T(i)< 10)
+        stepSize = T(i)/100;
+    else
+        stepSize = 0.1;
+    end
+    figure();
+    sim('single_object');
+    res = Y.signals.values;
+    [X,FVAL] = fmincon(@myfun1,[1 1 1],[],[],[],[],[0.01 0.01 0.01]);
+    num = X(1);
+    den = [X(2) 1 ];
+    delay = X(3);
+    sim('single_object');
+    subplot(2,1,1);
+    plot(Y.time,res,Y.time,Y.signals.values);
+    grid on;
+    title('Odpowiedzi skokowe obiektów');
+    legend('Rzeczywisty obiekt','Aproksymacja obiektu');
+    xlabel('Czas');
+    ylabel('Wartoœæ');
     subplot(2,1,2)
-    plot(Tg,Yg);
-end
-grid on;
-title('Odpowiedzi skokowe obiektu P1')
-xlabel('Czas');
-ylabel('Wartoœæ');
-
-
+    plot(Y.time,res - Y.signals.values);
+    title('Ró¿nicza odpowiedzi');
+    xlabel('Czas');
+    ylabel('Wartoœæ');
+    grid on;
+ end
 
 
 %% P2
 T = [0.01 0.02 0.05 0.1 0.2 0.3 0.5 0.7 1 1.3 1.5 2 4 6 8 10 20 50 100 200 500];
-for i = 1:length(T)
-    P2(i) = d * tf(1,[T(i) 1]) * tf(1,[T(i) 1]);
-end
 
-figure()
-hold on;
 for i = 1:length(T)
-    [Y,t] = step(P2(i),4000);
-    plot (t,Y);
-end
-grid on;
-title('Odpowiedzi skokowe obiektu P2')
-xlabel('Czas');
-ylabel('Wartoœæ');
+    num = 1;
+    den = [T(i)^2 2*T(i) 1];
+    delay = 1;
+    simTime = delay + 6*T(i);
+    if (T(i)< 10)
+        stepSize = T(i)/100;
+    else
+        stepSize = 0.1;
+    end
+    figure();
+    sim('single_object');
+    res = Y.signals.values;
+    [X,FVAL] = fmincon(@myfun1,[1 1 1],[],[],[],[],[0.01 T(i)/2 0.01]);
+    num = X(1);
+    den = [X(2) 1];
+    delay = X(3);
+    sim('single_object');
+    subplot(2,1,1);
+    plot(Y.time,res,Y.time,Y.signals.values);
+    grid on;
+    title('Odpowiedzi skokowe obiektów');
+    legend('Rzeczywisty obiekt','Aproksymacja obiektu');
+    xlabel('Czas');
+    ylabel('Wartoœæ');
+    subplot(2,1,2)
+    plot(Y.time,res - Y.signals.values);
+    title('Ró¿nicza odpowiedzi');
+    xlabel('Czas');
+    ylabel('Wartoœæ');
+    grid on;
+ end
 
 %% P3
 T=[0.005 0.01 0.02 0.05 0.1 0.2 0.5 2 5 10];
-for i = 1:length(T)
-    P3(i) = tf(1,[1 1]) * tf(1,[T(i) 1]) * tf(1,[T(i) 1]);
-end
 
-figure()
-hold on;
 for i = 1:length(T)
-    [Y,t] = step(P3(i),80);
-    plot (t,Y);
-end
-grid on;
-title('Odpowiedzi skokowe obiektu P3')
-xlabel('Czas');
-ylabel('Wartoœæ');
-
+    rep = tf(1,[1 1])*tf(1,[T(i) 1])*tf(1,[T(i) 1]); 
+    num = rep.num{1,1}; 
+    den = rep.den{1,1};
+    delay = 0;
+    simTime = 6 + 6*T(i);
+    if (T(i)< 10)
+        stepSize = T(i)/100;
+    else
+        stepSize = 0.1;
+    end
+    figure();
+    sim('single_object');
+    res = Y.signals.values;
+    [X,FVAL] = fmincon(@myfun1,[1 1 1],[],[],[],[],[0.01 T(i)/2 0.01]);
+    num = X(1);
+    den = [X(2) 1];
+    delay = X(3);
+    sim('single_object');
+    subplot(2,1,1);
+    plot(Y.time,res,Y.time,Y.signals.values);
+    grid on;
+    title('Odpowiedzi skokowe obiektów');
+    legend('Rzeczywisty obiekt','Aproksymacja obiektu');
+    xlabel('Czas');
+    ylabel('Wartoœæ');
+    subplot(2,1,2)
+    plot(Y.time,res - Y.signals.values);
+    title('Ró¿nicza odpowiedzi');
+    xlabel('Czas');
+    ylabel('Wartoœæ');
+    grid on;
+ end
 
 %% P4
 n = [3 4 5 6 7 8];
-for i = 1:length(n)
-    P4(i) = tf(1,[1 1])^n(i);
-end
 
-figure()
-hold on;
 for i = 1:length(n)
-    [Y,t] = step(P4(i),20);
-    plot (t,Y);
-end
-grid on;
-title('Odpowiedzi skokowe obiektu P4')
-xlabel('Czas');
-ylabel('Wartoœæ');
+    rep = tf(1,[1 1])^n(i);
+    num = rep.num{1,1}; 
+    den = rep.den{1,1};
+    delay = 0;
+    simTime = 6*n(i);
+    stepSize = 0.1;
+    figure();
+    sim('single_object');
+    res = Y.signals.values;
+    [X,FVAL] = fmincon(@myfun1,[1 1 1],[],[],[],[],[0.01 0.01 0.01]);
+    num = X(1);
+    den = [X(2) 1];
+    delay = X(3);
+    sim('single_object');
+    subplot(2,1,1);
+    plot(Y.time,res,Y.time,Y.signals.values);
+    grid on;
+    title('Odpowiedzi skokowe obiektów');
+    legend('Rzeczywisty obiekt','Aproksymacja obiektu');
+    xlabel('Czas');
+    ylabel('Wartoœæ');
+    subplot(2,1,2)
+    plot(Y.time,res - Y.signals.values);
+    title('Ró¿nicza odpowiedzi');
+    xlabel('Czas');
+    ylabel('Wartoœæ');
+    grid on;
+ end
 
 %% P5
 a = [0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9];
-for i = 1:length(a)
-    P5(i) = tf(1,[1 1]) * tf(1,[a(i) 1]) * tf(1,[a(i)^2 1]) * tf(1,[a(i)^3 1]);
-end
 
-figure()
-hold on;
 for i = 1:length(a)
-    [Y,t] = step(P5(i),12);
-    plot (t,Y);
-end
-grid on;
-title('Odpowiedzi skokowe obiektu P5')
-xlabel('Czas');
-ylabel('Wartoœæ');
+    rep = tf(1,[1 1])*tf(1,[a(i) 1])*tf(1,[a(i)^2 1])*tf(1,[a(i)^3 1]); 
+    num = rep.num{1,1}; 
+    den = rep.den{1,1};
+    delay = 0;
+    simTime = 6 + 6*a(i);
+    if (a(i)< 10)
+        stepSize = a(i)/100;
+    else
+        stepSize = 0.1;
+    end
+    figure();
+    sim('single_object');
+    res = Y.signals.values;
+    [X,FVAL] = fmincon(@myfun1,[1 1 1],[],[],[],[],[0.01 a(i)/2 0.01]);
+    num = X(1);
+    den = [X(2) 1];
+    delay = X(3);
+    sim('single_object');
+    subplot(2,1,1);
+    plot(Y.time,res,Y.time,Y.signals.values);
+    grid on;
+    title('Odpowiedzi skokowe obiektów');
+    legend('Rzeczywisty obiekt','Aproksymacja obiektu');
+    xlabel('Czas');
+    ylabel('Wartoœæ');
+    subplot(2,1,2)
+    plot(Y.time,res - Y.signals.values);
+    title('Ró¿nicza odpowiedzi');
+    xlabel('Czas');
+    ylabel('Wartoœæ');
+    grid on;
+ end
 
 %% P6
 L1 = [0.01 0.02 0.05 0.1 0.3 0.5 0.7 0.9 1];
-for i = 1:length(L1)
-    T1 = 1 -L1(i);
-    P6(i) = tf(1,[T1 1 0]) * exp(-tf('s')*L1(i));
-end
 
-figure()
-hold on;
 for i = 1:length(L1)
-    [Y,t] = step(P6(i),5);
-    plot (t,Y);
-end
-grid on;
-title('Odpowiedzi skokowe obiektu P6')
-xlabel('Czas');
-ylabel('Wartoœæ');
+    T1 = 1 - L1(i);
+    rep = tf(1,[1 0])*tf(1,[T1 1]); 
+    num = rep.num{1,1}; 
+    den = rep.den{1,1};
+    delay = L1(i);
+    simTime = 6+6*T1;
+    if (T1 < 10 & T1 ~= 0)
+        stepSize = T1/100;
+    else
+        stepSize = 0.1;
+    end
+    figure();
+    sim('single_object');
+    res = Y.signals.values;
+    [X,FVAL] = fmincon(@myfun1,[1 1 1],[],[],[],[],[0.01 0.01 0.01]);
+    num = X(1);
+    den = [X(2) 1];
+    delay = X(3);
+    sim('single_object');
+    subplot(2,1,1);
+    plot(Y.time,res,Y.time,Y.signals.values);
+    grid on;
+    title('Odpowiedzi skokowe obiektów');
+    legend('Rzeczywisty obiekt','Aproksymacja obiektu');
+    xlabel('Czas');
+    ylabel('Wartoœæ');
+    subplot(2,1,2)
+    plot(Y.time,res - Y.signals.values);
+    title('Ró¿nicza odpowiedzi');
+    xlabel('Czas');
+    ylabel('Wartoœæ');
+    grid on;
+ end
 
 
 %% P7
 T = [1 2 5 10];
 L1 = [0.01 0.02 0.05 0.1 0.3 0.5 0.7 0.9 1];
-for i = 1:length(T)
-    for j = 1:length(L1)
-        T1 = 1 -L1(j);
-        P7(i,j) = tf(T(i),[T(i) 1 ]) * tf(1,[T1 1]) * exp(-tf('s')*L1(i));
-    end
-end
 
-figure()
-hold on;
 for i = 1:length(T)
     for j = 1:length(L1)
-        [Y,t] = step(P7(i,j),60);
-        plot (t,Y);
+        T1 = 1 - L1(j);
+        rep = tf(T(i),[T(i) 1])*tf(1,[T1 1]); 
+        num = rep.num{1,1}; 
+        den = rep.den{1,1};
+        delay = L1(j);
+        simTime = 7*T(i) + 6*T1;
+        if (T1 ~= 0)
+            stepSize = T1/100;
+        else
+            stepSize = T(i)/100;
+        end
+        figure();
+        sim('single_object');
+        res = Y.signals.values;
+        [X,FVAL] = fmincon(@myfun1,[1 1 1],[],[],[],[],[0.01 T(i)/2 0.01]);
+        num = X(1);
+        den = [X(2) 1];
+        delay = X(3);
+        sim('single_object');
+        subplot(2,1,1);
+        plot(Y.time,res,Y.time,Y.signals.values);
+        grid on;
+        title('Odpowiedzi skokowe obiektów');
+        legend('Rzeczywisty obiekt','Aproksymacja obiektu');
+        xlabel('Czas');
+        ylabel('Wartoœæ');
+        subplot(2,1,2)
+        plot(Y.time,res - Y.signals.values);
+        title('Ró¿nicza odpowiedzi');
+        xlabel('Czas');
+        ylabel('Wartoœæ');
+        grid on;
     end
-end
-grid on;
-title('Odpowiedzi skokowe obiektu P7')
-xlabel('Czas');
-ylabel('Wartoœæ');
+ end
 
 %% P8
 a = [0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1 1.1];
-for i = 1:length(a)
-    P8(i) = tf([-a(i) 1], 1) * tf(1,[1 1])^3;
-end
 
-figure()
-hold on;
 for i = 1:length(a)
-    [Y,t] = step(P8(i),12);
-    plot (t,Y);
-end
-grid on;
-title('Odpowiedzi skokowe obiektu P8')
-xlabel('Czas');
-ylabel('Wartoœæ');
+    rep = tf([-a(i) 1],[1 1])*tf(1,[1 1])*tf(1,[1 1]); 
+    num = rep.num{1,1}; 
+    den = rep.den{1,1};
+    delay = 0;
+    simTime = 20;
+    stepSize = 0.01;
+    figure();
+    sim('single_object');
+    res = Y.signals.values;
+    [X,FVAL] = fmincon(@myfun1,[1 1 1],[],[],[],[],[0.01 0.01 0.01]);
+    num = X(1);
+    den = [X(2) 1];
+    delay = X(3);
+    sim('single_object');
+    subplot(2,1,1);
+    plot(Y.time,res,Y.time,Y.signals.values);
+    grid on;
+    title('Odpowiedzi skokowe obiektów');
+    legend('Rzeczywisty obiekt','Aproksymacja obiektu');
+    xlabel('Czas');
+    ylabel('Wartoœæ');
+    subplot(2,1,2)
+    plot(Y.time,res - Y.signals.values);
+    title('Ró¿nicza odpowiedzi');
+    xlabel('Czas');
+    ylabel('Wartoœæ');
+    grid on;
+ end
 
 %% P9
 T = [0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1];
-for i = 1:length(T)
-    P9(i) = tf(1,[1 1]) * tf(1,[T(i)^2 1.4*T(i) 1]);
-end
 
-figure()
-hold on;
 for i = 1:length(T)
-    [Y,t] = step(P9(i));
-    plot (t,Y);
-end
-grid on;
-title('Odpowiedzi skokowe obiektu P9')
-xlabel('Czas');
-ylabel('Wartoœæ');
+    rep = tf(1,[1 1])*tf(1,[T(i)^2 1.4*T(i) 1]); 
+    num = rep.num{1,1}; 
+    den = rep.den{1,1};
+    delay = 0;
+    simTime = 6 + 8*T(i);
+    stepSize = 0.01;
+    figure();
+    sim('single_object');
+    res = Y.signals.values;
+    [X,FVAL] = fmincon(@myfun1,[1 1 1],[],[],[],[],[0.01 T(i)/2 0.01]);
+    num = X(1);
+    den = [X(2) 1];
+    delay = X(3);
+    sim('single_object');
+    subplot(2,1,1);
+    plot(Y.time,res,Y.time,Y.signals.values);
+    grid on;
+    title('Odpowiedzi skokowe obiektów');
+    legend('Rzeczywisty obiekt','Aproksymacja obiektu');
+    xlabel('Czas');
+    ylabel('Wartoœæ');
+    subplot(2,1,2)
+    plot(Y.time,res - Y.signals.values);
+    title('Ró¿nicza odpowiedzi');
+    xlabel('Czas');
+    ylabel('Wartoœæ');
+    grid on;
+ end
